ed1f66f06d5288e04ffcdd63a341153e16440d44
quietpochatok <bosroin@gmail.com>
2020-06-24 20:14:48 +0500

    refactor: variable 'hero' init before run test

--- hero_spec.rb
+++ hero_spec.rb
@@ -1,24 +1,28 @@
 require './hero'
 
 describe Hero do
+
+  before do
+    @hero = Hero.new('mike')
+  end
   
   it "has a capitalize name" do 
-    hero = Hero.new('mike')
-      expect(hero.name).to eq 'Mike' #hero.name == Mike
+    
+      expect(@hero.name).to eq 'Mike' #hero.name == Mike
   end
 
   it "has a power_up" do
-    hero = Hero.new('mike')
-      expect(hero.power_up).to eq 110
+    
+      expect(@hero.power_up).to eq 110
   end
 
   it "has a power_down" do 
-    hero = Hero.new('mike')
-    expect(hero.power_down).to eq 90
+    
+    expect(@hero.power_down).to eq 90
   end
 
   it "has hero_info" do
-    hero = Hero.new('mike')
-    expect(hero.hero_info).to eq "Mike has a health of 100" 
+    
+    expect(@hero.hero_info).to eq "Mike has a health of 100" 
   end
 end
