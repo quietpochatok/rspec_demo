63f9ada97125833895ca6edbd20343ae876ea5e4
quietpochatok <bosroin@gmail.com>
2020-06-24 20:07:14 +0500

    feat: add test for hero_info

--- hero_spec.rb
+++ hero_spec.rb
@@ -11,4 +11,14 @@ describe Hero do
     hero = Hero.new('mike')
       expect(hero.power_up).to eq 110
   end
+
+  it "has a power_down" do 
+    hero = Hero.new('mike')
+    expect(hero.power_down).to eq 90
+  end
+
+  it "has hero_info" do
+    hero = Hero.new('mike')
+    expect(hero.hero_info).to eq "Mike has a health of 100" 
+  end
 end
